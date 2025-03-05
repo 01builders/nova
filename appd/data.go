f0cad87356445acd1f9e5fb730cfada1da2234b9
chatton <github.qpeyb@simplelogin.fr>
2025-03-05 12:23:25 +0000

    chore: add platform utility fn

--- appd/data.go
+++ appd/data.go
@@ -7,10 +7,16 @@ import (
 
 // CelestiaApp returns the compressed platform specific Celestia binary.
 func CelestiaApp() ([]byte, error) {
-	platform := runtime.GOOS + "_" + runtime.GOARCH // Check if we actually have binary data
+	// Check if we actually have binary data
 	if len(binaryCompressed) == 0 {
-		return nil, fmt.Errorf("no binary data available for platform %s", platform)
+		return nil, fmt.Errorf("no binary data available for platform %s", platform())
 	}
 
 	return binaryCompressed, nil
+}
+
+// platform returns a string representing the current operating system and architecture
+// This is useful for identifying platform-specific binaries or configurations.
+func platform() string {
+	return runtime.GOOS + "_" + runtime.GOARCH
 }
--- appd/data_test.go
+++ appd/data_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 func TestCelestiaApp(t *testing.T) {
+	t.Logf("running on platform: %s", platform())
 	data, err := CelestiaApp()
 	require.NoError(t, err, "CelestiaApp should not return an error")
 	require.NotNil(t, data, "CelestiaApp should return non-nil data")
