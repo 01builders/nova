f66fa85139b444aa9c45e702eaed3255d694947d
Julien Robert <julien@rbrt.fr>
2025-03-21 14:13:09 +0100

    cleanup

--- abci/errors.go
+++ abci/errors.go
@@ -5,7 +5,4 @@ import "errors"
 var (
 	// ErrNoVersionFound is returned when no remote version is found for a given height.
 	ErrNoVersionFound = errors.New("no version found")
-
-	// ErrInvalidArgument is returned when an invalid argument is provided.
-	ErrInvalidArgument = errors.New("invalid argument")
 )
